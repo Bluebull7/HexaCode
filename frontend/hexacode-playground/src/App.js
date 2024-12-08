dbca7301612e76f73f60bf2710b64bcf7ecd4d41
James H. <lsaucyfx@gmail.com>
2024-12-08 18:53:17 -0500

    updated menus and docs filename

--- frontend/hexacode-playground/src/App.js
+++ frontend/hexacode-playground/src/App.js
@@ -8,14 +8,26 @@ import Sidebar from './components/Sidebar2';
 import AppHeader from './components/Header';
 import 'antd/dist/reset.css'; // For Ant Design styles
 
-const { Content, Sider, Footer } = Layout;
+const { Header, Content, Sider, Footer } = Layout;
 
 const App = () => {
   return (
     <Router>
       <Layout style={{ minHeight: '100vh' }}>
         {/* Application Header */}
-        <AppHeader />
+        <Header>
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['home']}>
+            <Menu.Item key="home">
+              <Link to="/">Home</Link>
+            </Menu.Item>
+            <Menu.Item key="playground">
+              <Link to="/playground">Playground</Link>
+            </Menu.Item>
+            <Menu.Item key="docs">
+              <Link to="/docs">Documentation</Link>
+            </Menu.Item>
+          </Menu>
+        </Header>
 
         <Layout>
           {/* Sidebar for Documentation Navigation */}
@@ -33,7 +45,7 @@ const App = () => {
               <Route path="/playground" element={<Playground />} />
 
               {/* Documentation Route */}
-              <Route path="/docs" element={<Docs />} />
+              <Route path="/docs/*" element={<Docs />} />
 
               {/* Redirect Invalid Routes */}
               <Route path="*" element={<Navigate to="/" />} />
--- frontend/hexacode-playground/src/components/Sidebar2.js
+++ frontend/hexacode-playground/src/components/Sidebar2.js
@@ -6,13 +6,13 @@ const Sidebar2 = () => {
   return (
     <Menu mode="inline" style={{ height: '100%' }}>
       <Menu.Item key="introduction">
-        <Link to="/docs/introduction">Introduction</Link>
+        <Link to="/introduction.html">Introduction</Link>
       </Menu.Item>
       <Menu.Item key="getting-started">
-        <Link to="/docs/getting-started">Getting Started</Link>
+        <Link to="/DOCS.html">Getting Started</Link>
       </Menu.Item>
       <Menu.Item key="api">
-        <Link to="/docs/api">API Reference</Link>
+        <Link to="/api-reference.html">API Reference</Link>
       </Menu.Item>
     </Menu>
   );
